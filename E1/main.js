7c8a26e47657e27e6cd818a364dbc30c699b17bf
Franco Nardella <fnardella@godoycruz.gov.ar>
2023-11-14 11:48:17 -0300

    Creacion Repo

---
+++ E1/main.js
@@ -0,0 +1,16 @@
+// Ejercicio 1:
+
+// const ParImpar = (num) => {
+//   if (num % 2 === 0) {
+//     console.log(`El numero ${num} es par`);
+//   } else {
+//     console.log(`El numero ${num} es impar`);
+//   }
+// };
+
+// ParImpar(1);
+// ParImpar(2);
+// ParImpar(3);
+// ParImpar(4);
+
+// Ejercicio 2:
